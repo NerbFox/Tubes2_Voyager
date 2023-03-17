7604789178aecc5f410f60e1e677b145ac3157d7
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-03-17 23:23:06 +0700

    Update main.cs

--- src/main.cs
+++ src/main.cs
@@ -1,4 +1,7 @@
 using System;
+using Algorithm;
+using inputHandler;
+using myMap;
 
 namespace Main
 {
@@ -7,6 +10,16 @@ namespace Main
     static void Main(string[] args)
     {
       Console.WriteLine("Ghazi Akmal Fauzan");    
+      Console.WriteLine("Tubes 2");
+      Console.WriteLine("Voyager");
+      Console.WriteLine("====================================");
+      Console.WriteLine("1. BFS");
+      Console.WriteLine("2. DFS");
+      DFS dfs = new DFS();
+      BFS bfs = new BFS();
+      Console.Write("Pilih Algoritma: ");
+      dfs.print();
+      bfs.print();
     }
   }
 }
