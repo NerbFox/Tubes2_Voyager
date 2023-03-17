d32bed4bd71e185053146f6662a6d7e7fb6b91ca
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-03-17 22:43:12 +0700

    driver map

---
+++ test/driver/map/driver.cs
@@ -0,0 +1,24 @@
+using System;
+using inputHandler;
+using myMap;
+
+class driverMap
+{
+    static void Main(string[] args)
+    {
+        inputHandlerFile file = new inputHandlerFile();
+        file.readFile("../../testInput3.txt");
+        map map = new map(file.getInputData());
+        Console.WriteLine(map.getMapWidth());
+        Console.WriteLine(map.getMapHeight());
+        Console.WriteLine(map.getMapSize());
+        char[,] data = map.getMapData();
+        for (int row = 0; row < data.GetLength(0); row++) {
+            for (int col = 0; col < data.GetLength(1); col++) {
+                Console.Write(data[row, col] + " ");
+            }
+            Console.WriteLine();
+        }
+        map.print();
+    }
+}
