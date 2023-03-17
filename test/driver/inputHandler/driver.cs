7fca574e3c84db0374b8fc7ec08d74aa4430be3c
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-03-17 22:42:57 +0700

    Update driver inputHandler

--- test/driver/inputHandler/driver.cs
+++ test/driver/inputHandler/driver.cs
@@ -1,24 +1,22 @@
 using System;
+using inputHandler;
 
-namespace inputHandler
+class driverInputHandler
 {
-    class driverInputHandler
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            inputHandlerFile file = new inputHandlerFile();
-            inputHandlerCommand command = new inputHandlerCommand();
-            file.ReadFile("../../testInput.txt");
-            string[] data = file.getInputData();
-            for (int i = 0; i < data.Length; i++) {
-                Console.WriteLine(data[i]);
-            }
-            file.getNumberOfLines();
-            file.getNumberOfRows();
-
-            Console.WriteLine("Command Input:");
-            command.input(1, 3);
-            Console.WriteLine(command.getCommand());
+        inputHandlerFile file = new inputHandlerFile();
+        inputHandlerCommand command = new inputHandlerCommand();
+        file.readFile("../../testInput2.txt");
+        string[] data = file.getInputData();
+        for (int i = 0; i < data.Length; i++) {
+            Console.WriteLine(data[i]);
         }
+        file.getNumberOfLines();
+        file.getNumberOfRows();
+
+        Console.WriteLine("Command Input:");
+        command.input(1, 3);
+        Console.WriteLine(command.getCommand());
     }
 }
