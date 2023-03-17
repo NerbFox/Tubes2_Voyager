205f0b735d90870f2eaf2c66244248132340d8f6
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-03-17 20:47:46 +0700

    Cleaning main branch

--- Makefile
+++
@@ -1,8 +0,0 @@
-build:
-	@cd ./src && dotnet build --nologo && cd ..
-
-run:
-	@cd ./src && dotnet run && cd ..
-
-clean:
-	@cd ./src && dotnet clean --nologo && cd ..
--- src/class/algorithm/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-namespace Main;
-public class Algorithm {
-    public void print(){
-        Console.WriteLine("Hello, World!");
-    }
-}
