a4361c21ea847733da8477679ddcc9794746fba6
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-03-17 23:22:52 +0700

    Create driver.cs

---
+++ test/driver/BFS_DFS/driver.cs
@@ -0,0 +1,13 @@
+using System;
+using Algorithm;
+
+class driverDFS_BFS
+{
+    static void Main(string[] args)
+    {
+        BFS bfs = new BFS();
+        bfs.print();
+        DFS dfs = new DFS();
+        dfs.print(); 
+    }
+}
