39e7dc0e2ab2b225fc88148b62dd024ef9426c01
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-03-17 23:23:13 +0700

    Create DFS.cs

---
+++ src/class/DFS.cs
@@ -0,0 +1,38 @@
+// program for DFS Voyager with a class
+using System;
+namespace Algorithm{
+    public class DFS
+    {
+        // // instan
+        int treasure;
+        // char[,] Map;
+        // private Graph G;
+        // private boolean[] visited;
+        // // constructor
+        // public Algorithm(Graph G)
+        // {
+        //     // initialize instance variables
+        //     this.G = G;
+        //     visited = new boolean[G.V()];
+        // }
+
+        public void DFSMethod(int v, char Map)
+        {
+            // // mark v as visited
+            // visited[v] = true;
+            // // for each vertex w adjacent to v
+            // for (int w : G.adj(v))
+            // {
+            //     // if w is not visited
+            //     if (!visited[w])
+            //     {
+            //         // recursively visit w
+            //         DFS(w);
+            //     }
+            // }
+        }
+        public void print(){
+            Console.WriteLine("Hello DFS");
+        }
+    }
+};
