b9bc9e6116ea7b56577322c3174ced4081acf390
NerbFox <13521043@std.stei.itb.ac.id>
2023-03-20 19:41:16 +0700

    Update dfs

--- src/class/Graph.cs
+++ src/class/Graph.cs
@@ -5,11 +5,13 @@ struct mapElmt
     public int index;
     public int row;
     public int col;
+    public int n_visited;
     public mapElmt(int index, int row, int col)
     {
         this.index = index;
         this.row = row;
         this.col = col;
+        this.n_visited = 0;
     }
 }
 namespace Graph{
@@ -22,19 +24,29 @@ namespace Graph{
         // constructor
         public MyGraph(int vertices)
         {
-            v = vertices;
+            this.v = vertices;
             adj = new List<mapElmt>[v];
             for (int i = 0; i < v; ++i)
                 adj[i] = new List<mapElmt>();
             // adj = new List<mapElmt>[4];
             // for (int i = 0; i < 4; ++i)
             //     adj[i] = new List<mapElmt>();
+
+            // adj = new List<List<mapElmt>>(vertices);
+            // visited = new bool[vertices]; // set the size of the "visited" array to the total number of vertices
+            // for (int i = 0; i < vertices; i++)
+            // {
+            //     adjList.Add(new List<mapElmt>());
+            //     visited[i] = false;
+            // }
         }
 
         // add an edge to the graph
         public void AddEdge(int u, mapElmt v)
         {
             adj[u].Add(v);
+            Console.WriteLine(u + " " + v.index + " " + v.row + " " + v.col);
+            // print();    
             // adj[u].Add((v1,v2));
         }
 
@@ -52,16 +64,20 @@ namespace Graph{
         public void DFS(mapElmt start)
         {
             bool[] visited = new bool[v]; // keep track of visited vertices
+            for (int i = 0; i < v; i++)
+                visited[i] = false; // set all vertices to false (not visited
             Stack<(mapElmt, int)> DFSStack = new Stack<(mapElmt, int)>(); // DFSStack for DFS
 
             visited[start.index] = true; // set to true start vertex (visited)
             DFSStack.Push((start, 0)); // push start vertex ke DFSStack
+            // Console.Write(DFSStack.Count);
 
             while (DFSStack.Count != 0)
             {
                 (mapElmt current, int depth) = DFSStack.Pop(); // pop DFSStack
 
-                Console.Write(current.index + " "); // print the vertex
+                Console.Write(current.index  + " " + current.row + " " + current.col + " -> "); // print the vertex
+                // Console.Write(   "uyy");
 
                 // visit semua adjacent vertices dari vertex saat ini
                 foreach (mapElmt i in adj[current.index])
--- test/driver/Graph/Driver.cs
+++ test/driver/Graph/Driver.cs
@@ -12,22 +12,25 @@ class DriverGraph
         inputHandlerFile.readFile("../../TestInput3.txt");
         MyMap map = new MyMap(inputHandlerFile.getInputData());
         // DFS dfs = new DFS(map);
-        int v = -1;
+        int v = 0;
         int row = 0;
         int col = 0;
-        int ind = 0;
+        int ind = 9;
         for (int i = 0; i < map.getMapHeight(); i++)
         {
             for (int j = 0; j < map.getMapWidth(); j++)
             {
-                if (map.getElement(i, j) != 'X'){
+                if (map.getElement(i, j) != 'X')
+                {
                     v++;
                 }
                 Console.Write(map.getElement(i, j) + " ");
-                if (map.getElement(i, j) == 'K'){
+                if (map.getElement(i, j) == 'K')
+                {
+                    Console.WriteLine("K");
                     row = i;
                     col = j;
-                    ind = v;
+                    ind = v-1;
                 }
             }
             Console.WriteLine();
@@ -39,36 +42,48 @@ class DriverGraph
         {
             for (int j = 0; j < map.getMapWidth(); j++)
             {
-                if (map.getElement(i, j) != 'X'){
+                if (map.getElement(i, j) != 'X')
+                {
                     // mapElmt m = new mapElmt(index, i, j);
-                    if (i > 0 && map.getElement(i - 1, j) != 'X'){
+                    if (i > 0 && map.getElement(i - 1, j) != 'X')
+                    {
                         mapElmt m1 = new mapElmt(index - map.getMapWidth(), i - 1, j);
                         g.AddEdge(index, m1);
                     }
-                    if (i < map.getMapHeight() - 1 && map.getElement(i + 1, j) != 'X'){
+                    if (i < map.getMapHeight() - 1 && map.getElement(i + 1, j) != 'X')
+                    {
                         mapElmt m2 = new mapElmt(index + map.getMapWidth(), i + 1, j);
                         g.AddEdge(index, m2);
                     }
-                    if (j > 0 && map.getElement(i, j - 1) != 'X'){
-                        mapElmt m3 = new mapElmt(index-1, i, j - 1);
+                    if (j > 0 && map.getElement(i, j - 1) != 'X')
+                    {
+                        mapElmt m3 = new mapElmt(index - 1, i, j - 1);
                         g.AddEdge(index, m3);
                     }
-                    if (j < map.getMapWidth() - 1 && map.getElement(i, j + 1) != 'X'){
-                        mapElmt m4 = new mapElmt(index+1, i, j + 1);
+                    if (j < map.getMapWidth() - 1 && map.getElement(i, j + 1) != 'X')
+                    {
+                        mapElmt m4 = new mapElmt(index + 1, i, j + 1);
                         g.AddEdge(index, m4);
                     }
-                    index++;
                     // v++;
                 }
+                index++;
+                // Console.WriteLine(index);
             }
         }
         mapElmt start = new mapElmt(ind, row, col);
-        Console.WriteLine(index);
-        g.print();
+        // g.addEdge(start);
         Console.WriteLine("ind = " + ind);
+        Console.WriteLine("row = " + row);
+        Console.WriteLine("col = " + col);
+        Console.WriteLine(index);
+        Console.WriteLine(ind);
+        g.print();
+        // Console.WriteLine("ind = " + ind);
+        map.print();
         Console.WriteLine("\n\nDepth-first traversal (starting from vertex start):");
         g.DFS(start);
-        
+
         // mapElmt m = new mapElmt(i, j, map.getElement(i, j));
         // MyGraph g = new MyGraph(4);
         // mapElmt m1 = new mapElmt(0, 90, 100);
@@ -81,7 +96,7 @@ class DriverGraph
         // g.AddEdge(2, m3);
         // g.AddEdge(3, m2);
         // Console.WriteLine("Depth-first traversal (starting from vertex 2):");
-        // g.DFS(m2);
+        // g.DFS(m1);
         // Console.WriteLine("\n\ngraph:");
         // g.print();
     }
